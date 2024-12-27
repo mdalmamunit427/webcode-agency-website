8994faee78c3f0f7f9db17a21d13a5ef11fd76f5
mdalmamunit427 <mdalmamunit427@gmail.com>
2024-12-27 22:08:55 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.envVITE_APPID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
